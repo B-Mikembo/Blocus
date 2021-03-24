2a8233fd45868e2944995104c946697f43d7e069
B-Mikembo <brice.mikembo@etu.u-pec.fr>
2021-03-24 16:18:22 +0100

    correction

--- includes/menu.h
+++ includes/menu.h
@@ -5,17 +5,6 @@
 
 #include <MLV/MLV_all.h>
 
-typedef struct{
-    int x_pos;
-    int y_pos;
-} Cell_Menu;
-
-typedef struct{
-    Cell_Menu **cells;
-    int x_size;
-    int y_size;
-} Grid_Menu;
-
 void menu_window();
 void free_grid_menu(Grid_Menu *grid_menu);
 void display_username( int width, int height, char* texte1, char* texte2 );
