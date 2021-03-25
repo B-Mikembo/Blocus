be8dd4bde2f9ddf552f8d27d8282562d5bcaac50
B-Mikembo <brice.mikembo@etu.u-pec.fr>
2021-03-25 10:30:41 +0100

    wip

---
+++ includes/game.h
@@ -0,0 +1,16 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <MLV/MLV_all.h>
+
+typedef struct{
+
+} Cell;
+
+typedef struct{
+
+} Grid;
+
+void game_window(char *name_player1, char *name_player2, int grid_size);
+
+#endif
---
+++ src/game.c
@@ -0,0 +1,5 @@
+#include "../includes/game.h"
+
+void game_window(){
+    
+}
