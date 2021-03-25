08ccfd5570537fa0758753727fdab88c1768838c
B-Mikembo <brice.mikembo@etu.u-pec.fr>
2021-03-25 09:55:43 +0100

    wip

--- includes/window.h
+++ includes/window.h
@@ -10,12 +10,14 @@ typedef struct {
     int height;
     MLV_Image *image;
     int value;
+    int enable;
+    char *message;
 } Button;
 
 void create_window(const char* title, unsigned int width, unsigned int height);
 int is_pressed_escape();
 void close_window();
-void draw_Buttons(Button *first_button, Button *second_button);
+void draw_buttons(Button *first_button, Button *second_button, Button *play);
 void free_image(MLV_Image *image);
 
 #endif
