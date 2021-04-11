1072f113d6cee702ababa8095123792172faf37a
B-Mikembo <brice.mikembo@etu.u-pec.fr>
2021-04-11 18:16:08 +0200

    wip

--- includes/game.h
+++ includes/game.h
@@ -19,17 +19,18 @@ typedef struct
     char *name;
     int numPlayer;
     MLV_Image *image;
+    MLV_Image *cross;
     PositionPlayer *position;
 } Player;
 
 void displayMap(int **map, int size);
 int **createMap(int size);
-Player *createPlayer(char *name, int num, MLV_Image *image);
+Player *createPlayer(char *name, int num, MLV_Image *image, MLV_Image *crossImage);
 void game_window(char *name_player1, char *name_player2, int grid_size);
 int isNearbyCase(Player *player, int x, int y);
 void movePlayer(int **map, Player *player, int size);
 int isOutsideMap(int x, int y, int size);
-void condamnationStep(Player *player);
+void condamnationPlayer(int **map, int size, Player *player);
 void placementPlayer(Player *player, int **map, int size);
 void drawMap(int **map, int size);
 void drawRectangle(int x, int y);
